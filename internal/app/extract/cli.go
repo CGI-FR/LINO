ffc72cf49dda8e8627b20c5aafff150a8ae155cc
Youen Péron <youen.peron@cgi.com>
2019-12-18 16:22:29 +0100

    [extract] filter first table's row

--- internal/app/extract/cli.go
+++ internal/app/extract/cli.go
@@ -23,6 +23,7 @@ var extractExporter extract.RowExporter
 
 // local flags
 var limit uint
+var pk int
 
 var logger extract.Logger
 
@@ -71,6 +72,7 @@ func NewCommand(fullName string, err *os.File, out *os.File, in *os.File) *cobra
 		},
 	}
 	cmd.Flags().UintVarP(&limit, "limit", "l", 1, "limit the number of results")
+	cmd.Flags().IntVarP(&pk, "filter", "f", -1, "filter on primary key of start table")
 	cmd.SetOut(out)
 	cmd.SetErr(err)
 	cmd.SetIn(in)
@@ -111,9 +113,15 @@ func getExtractionPlan() (extract.Plan, *extract.Error) {
 	if err3 != nil {
 		return nil, &extract.Error{Description: err3.Error()}
 	}
+	var filter extract.Filter
 
-	filter := extract.NewFilter(limit, extract.Row{})
 	stepList := getStepList(ep, relations, tables)
+
+	if pk == -1 {
+		filter = extract.NewFilter(limit, extract.Row{})
+	} else {
+		filter = extract.NewFilter(limit, extract.Row{stepList.Step(0).Entry().PrimaryKey(): pk})
+	}
 
 	return extract.NewPlan(filter, stepList), nil
 }

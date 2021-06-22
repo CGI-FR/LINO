c443b0e553afdfa53e2ec477c7e15801869c93e0
Adrien Aury <adrien.aury@cgi.com>
2021-06-22 14:59:27 +0200

    feat(db2): add pull

---
+++ internal/infra/pull/datasource_db2.go
@@ -0,0 +1,35 @@
+package pull
+
+import (
+	"fmt"
+
+	"github.com/cgi-fr/lino/pkg/pull"
+)
+
+// Db2DataSourceFactory exposes methods to create new Db2 pullers.
+type Db2DataSourceFactory struct{}
+
+// NewDb2DataSourceFactory creates a new oracle datasource factory.
+func NewDb2DataSourceFactory() *Db2DataSourceFactory {
+	return &Db2DataSourceFactory{}
+}
+
+// New return a Db2 puller
+func (e *Db2DataSourceFactory) New(url string, schema string) pull.DataSource {
+	return &SQLDataSource{
+		url:     url,
+		schema:  schema,
+		dialect: Db2Dialect{},
+	}
+}
+
+// PostgresDialect implement postgres SQL variations
+type Db2Dialect struct{}
+
+func (od Db2Dialect) Placeholder(position int) string {
+	return "?"
+}
+
+func (od Db2Dialect) Limit(limit uint) string {
+	return fmt.Sprintf(" LIMIT %d", limit)
+}

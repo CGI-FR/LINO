4ecef148fe95ab3ec05f34664fdafd233cae233b
Adrien Aury <44274230+adrienaury@users.noreply.github.com>
2022-02-23 19:13:45 +0100

    Feat: table configuration commands (#65)
    * feat(addcolumn): add venom tests
    
    * feat(addcolumn): fix venom tests
    
    * feat(addcolumn): implement feature
    
    * feat(addcolumn): add venom test
    
    * feat(addcolumn): implement remove-column
    
    * docs(addcolumn): update changelog

--- internal/app/table/cli.go
+++ internal/app/table/cli.go
@@ -49,6 +49,8 @@ func NewCommand(fullName string, err *os.File, out *os.File, in *os.File) *cobra
 		Aliases: []string{"tab"},
 	}
 	cmd.AddCommand(newExtractCommand(fullName, err, out, in))
+	cmd.AddCommand(newAddColumnCommand(fullName, err, out, in))
+	cmd.AddCommand(newRemoveColumnCommand(fullName, err, out, in))
 	cmd.SetOut(out)
 	cmd.SetErr(err)
 	cmd.SetIn(in)
--- pkg/table/driver.go
+++ pkg/table/driver.go
@@ -17,6 +17,8 @@
 
 package table
 
+import "fmt"
+
 // Extract table metadatas from a relational database.
 func Extract(e Extractor, s Storage) *Error {
 	tables, err := e.Extract()
@@ -28,4 +30,121 @@ func Extract(e Extractor, s Storage) *Error {
 		return err
 	}
 	return nil
+}
+
+// AddOrUpdateColumn will update table definitions with given export and import types, it will add the column if necessary
+func AddOrUpdateColumn(s Storage, tableName, columnName, exportType, importType string) (int, *Error) {
+	tables, err := s.List()
+	if err != nil {
+		return 0, err
+	}
+
+	count := 0
+
+	updatedTables := []Table{}
+	for _, table := range tables {
+		if table.Name == tableName {
+			updatedTables = append(updatedTables, addOrUpdateColumn(table, columnName, exportType, importType))
+			count++
+		} else {
+			updatedTables = append(updatedTables, table)
+		}
+	}
+
+	if count == 0 {
+		return 0, &Error{Description: fmt.Sprintf("there is no table named %v", tableName)}
+	}
+
+	if err := s.Store(updatedTables); err != nil {
+		return count, err
+	}
+
+	return count, nil
+}
+
+func addOrUpdateColumn(table Table, columnName, exportType, importType string) Table {
+	count := 0
+
+	updatedColumns := []Column{}
+
+	for _, column := range table.Columns {
+		if column.Name == columnName {
+			exportUpdate := column.Export
+			if exportType != "" {
+				exportUpdate = exportType
+			}
+			importUpdate := column.Import
+			if importType != "" {
+				importUpdate = importType
+			}
+			updatedColumns = append(updatedColumns, Column{
+				Name:   columnName,
+				Export: exportUpdate,
+				Import: importUpdate,
+			})
+			count++
+		} else {
+			updatedColumns = append(updatedColumns, column)
+		}
+	}
+
+	if count == 0 {
+		updatedColumns = append(updatedColumns, Column{
+			Name:   columnName,
+			Export: exportType,
+			Import: importType,
+		})
+	}
+
+	table.Columns = updatedColumns
+
+	return table
+}
+
+// RemoveColumn will update table definitions removing specified column
+func RemoveColumn(s Storage, tableName, columnName string) (int, *Error) {
+	tables, err := s.List()
+	if err != nil {
+		return 0, err
+	}
+
+	count := 0
+
+	updatedTables := []Table{}
+	for _, table := range tables {
+		if table.Name == tableName {
+			updatedTables = append(updatedTables, removeColumn(table, columnName))
+			count++
+		} else {
+			updatedTables = append(updatedTables, table)
+		}
+	}
+
+	if count == 0 {
+		return 0, &Error{Description: fmt.Sprintf("there is no table named %v", tableName)}
+	}
+
+	if err := s.Store(updatedTables); err != nil {
+		return count, err
+	}
+
+	return count, nil
+}
+
+func removeColumn(table Table, columnName string) Table {
+	count := 0
+
+	updatedColumns := []Column{}
+
+	for _, column := range table.Columns {
+		if column.Name == columnName {
+			count++
+		} else {
+			updatedColumns = append(updatedColumns, column)
+		}
+	}
+
+	table.Columns = updatedColumns
+
+	return table
 }

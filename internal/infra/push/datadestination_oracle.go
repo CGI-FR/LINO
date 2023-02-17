91a714c05cd05d027aeb38e384a85109212246c7
Adrien Aury <adrien.aury@cgi.com>
2023-02-17 14:16:48 +0100

    Revert "fix(oracle): disable contraints only select FK"
    This reverts commit 4feebf61de618059f3a860001d33ae2250785fe0.

--- internal/infra/push/datadestination_oracle.go
+++ internal/infra/push/datadestination_oracle.go
@@ -45,7 +45,6 @@ func (d OracleDialect) EnableConstraintsStatement(tableName string) string {
 	 AND c.owner = sys_context( 'userenv', 'current_schema' )
 	 AND c.table_name = '%s'
 	 AND c.status = 'DISABLED'
-	 AND c.constraint_type = 'R'
 	 ORDER BY c.constraint_type)
 	LOOP
 	  dbms_utility.exec_ddl_statement('alter table "' || c.owner || '"."' || c.table_name || '" enable constraint ' || c.constraint_name);
@@ -62,7 +61,6 @@ func (d OracleDialect) EnableConstraintsStatement(tableName string) string {
 		 AND c.owner = '%s'
 		 AND c.table_name = '%s'
 		 AND c.status = 'DISABLED'
-		 AND c.constraint_type = 'R'
 		 ORDER BY c.constraint_type)
 		LOOP
 		  dbms_utility.exec_ddl_statement('alter table "' || c.owner || '"."' || c.table_name || '" enable constraint ' || c.constraint_name);
@@ -87,7 +85,7 @@ func (d OracleDialect) DisableConstraintsStatement(tableName string) string {
 		AND c.owner = sys_context( 'userenv', 'current_schema' )
 		AND c.table_name = '%s'
 		AND c.status = 'ENABLED'
-		AND c.constraint_type = 'R'
+		AND NOT (t.iot_type IS NOT NULL AND c.constraint_type = 'P')
 		ORDER BY c.constraint_type DESC)
 	LOOP
 		dbms_utility.exec_ddl_statement('alter table "' || c.owner || '"."' || c.table_name || '" disable constraint ' || c.constraint_name);
@@ -103,7 +101,7 @@ func (d OracleDialect) DisableConstraintsStatement(tableName string) string {
 		AND c.owner = '%s'
 		AND c.table_name = '%s'
 		AND c.status = 'ENABLED'
-		AND c.constraint_type = 'R'
+		AND NOT (t.iot_type IS NOT NULL AND c.constraint_type = 'P')
 		ORDER BY c.constraint_type DESC)
 	LOOP
 		dbms_utility.exec_ddl_statement('alter table "' || c.owner || '"."' || c.table_name || '" disable constraint ' || c.constraint_name);

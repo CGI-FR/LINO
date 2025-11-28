921f64066aae31eda9b5c67d02ad41e3ebb3f4a3
Adrien Aury <adrien.aury@cgi.com>
2025-11-28 11:42:04 +0100

    fix: revert postgresql extractor

--- internal/infra/relation/extractor_postgres.go
+++ internal/infra/relation/extractor_postgres.go
@@ -21,6 +21,8 @@ import (
 
 	// import postgresql connector
 
+	"fmt"
+
 	_ "github.com/lib/pq"
 
 	"github.com/cgi-fr/lino/pkg/relation"
@@ -44,46 +46,25 @@ type PostgresDialect struct{}
 func (d PostgresDialect) SQL(schema string) string {
 	SQL := `
 SELECT
-    fk.conname AS relation_name,
-    child.relname AS child_table,
-    att_child.attname AS child_column,
-    parent.relname AS parent_table,
-    att_parent.attname AS parent_column
+    tc.constraint_name,
+    tc.table_name,
+    kcu.column_name,
+    ccu.table_name AS foreign_table_name,
+    ccu.column_name AS foreign_column_name
 FROM
-    pg_constraint fk
-    -- Jointure sur le schéma parent et enfant
-JOIN
-    pg_class child ON fk.conrelid = child.oid
-JOIN
-    pg_class parent ON fk.confrelid = parent.oid
-    -- Jointure pour l'alignement des colonnes (index correspond à index)
-JOIN
-    unnest(fk.conkey) WITH ORDINALITY AS att_child_pos (attid, pos)
-    ON att_child_pos.pos <= array_length(fk.conkey, 1)
-JOIN
-    unnest(fk.confkey) WITH ORDINALITY AS att_parent_pos (attid, pos)
-    ON att_parent_pos.pos = att_child_pos.pos
-    -- Récupération du nom des colonnes enfants
-JOIN
-    pg_attribute att_child ON att_child.attrelid = child.oid
-    AND att_child.attnum = att_child_pos.attid
-    -- Récupération du nom des colonnes parentes
-JOIN
-    pg_attribute att_parent ON att_parent.attrelid = parent.oid
-    AND att_parent.attnum = att_parent_pos.attid
-WHERE
-    fk.contype = 'f' -- 'f' pour Foreign Key
-`
-	if schema != "" {
-		SQL += `AND child.relnamespace = (SELECT oid FROM pg_namespace WHERE nspname = '` + schema + `')`
-	}
+    information_schema.table_constraints AS tc
+    JOIN information_schema.key_column_usage AS kcu
+      ON tc.constraint_name = kcu.constraint_name
+      AND tc.table_schema = kcu.table_schema
+    JOIN information_schema.constraint_column_usage AS ccu
+      ON ccu.constraint_name = tc.constraint_name
+      AND ccu.table_schema = tc.table_schema
+WHERE tc.constraint_type = 'FOREIGN KEY'
+            `
 
-	SQL += `
-ORDER BY
-    child.relname,
-    fk.conname,
-    att_child_pos.pos
-`
+	if schema != "" {
+		SQL += fmt.Sprintf("AND tc.table_schema = '%s'", schema)
+	}
 
 	return SQL
 }
